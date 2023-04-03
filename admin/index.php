baeb7bbef02bd5aed0f52a04d6be54b5d8559290
rohit <rhtry.tech@gmail.com>
2023-04-04 02:12:41 +0530

    update :

--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,7 @@
   <div class="main-container container">
     <div class="content">
       <h2 style="font-weight: 500; text-align: center;">
-        <b>Dashboard</b>
+        <b>Grievance Cell | Dashboard</b>
       </h2>
       <!-- sessionalMessages -->
       <div>
@@ -31,45 +31,82 @@
           unset($_SESSION['login']);
         } ?>
       </div>
+      <br />
       <div class="table-responsive">
         <table class="table">
           <thead>
             <tr>
               <th>
-                <h4><b>Admins</b></h4>
+                <h5><b>Total Admins</b></h5>
               </th>
               <th>
-                <h4><b>Users</b></h4>
+                <h5><b>Total Users</b></h5>
               </th>
               <th>
-                <h4><b>Categories</b></h4>
+                <h5><b>Total Categories</b></h5>
               </th>
               <th>
-                <h4><b>Complains</b></h4>
+                <h5><b>Pending Complain</b></h5>
+              </th>
+              <th>
+                <h5><b>Resolved Complain</b></h5>
               </th>
             </tr>
           </thead>
+          <?php
+          // fetchingTotalNumberOfAdmins
+          $sql_admin = 'SELECT * FROM tbl_admin';
+          $res_admin = mysqli_query($conn, $sql_admin);
+          if ($res_admin == true) {
+            $count_admin = mysqli_num_rows($res_admin);
+          }
+
+          // fetchingTotalNumberOfUsers
+          $sql_user = 'SELECT * FROM tbl_user';
+          $res_user = mysqli_query($conn, $sql_user);
+          if ($res_user == true) {
+            $count_user = mysqli_num_rows($res_user);
+          }
+
+          // fetchingTotalCategories
+          $sql_category = 'SELECT * FROM tbl_complain_category';
+          $res_category = mysqli_query($conn, $sql_category);
+          if ($res_category == true) {
+            $count_category = mysqli_num_rows($res_category);
+          }
+
+          // fetchingTotalComplains
+          $sql_complain = 'SELECT * FROM tbl_complain';
+          $res_complain = mysqli_query($conn, $sql_complain);
+          if ($res_complain == true) {
+            $count_complain = mysqli_num_rows($res_complain);
+          }
+          ?>
           <tbody>
             <tr>
               <td>
                 <h3>
-                  5
+                  <?php echo $count_admin; ?>
                 </h3>
               </td>
               <td>
-
                 <h3>
-                  5 </h3>
+                  <?php echo $count_user; ?>
+                </h3>
               </td>
               <td>
-
                 <h3>
-                  5 </h3>
+                  <?php echo $count_category; ?>
+                </h3>
               </td>
               <td>
-
                 <h3>
-                  5
+                  <?php echo $count_complain; ?>
+                </h3>
+              </td>
+              <td>
+                <h3>
+                  <?php echo $count_complain; ?>
                 </h3>
               </td>
             </tr>
